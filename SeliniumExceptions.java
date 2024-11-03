2dc892470d209dbfb83708b395f18a6bf149bd16
Sukhaveni <sukhavenic@gmail.com>
2024-11-04 02:27:58 +0530

    Selinium exceptions file has been added

---
+++ SeliniumExceptions.java
@@ -0,0 +1,15 @@
+package SeliniumNotebook;
+
+// Exceptions
+
+// No: 1 - Invalid argument exception - URL is wrong(Always Url should be absolute)
+
+// No:2--- Session not created exception - Your Webdriver & browsers are mismatching
+
+//No :3 ----No such Element exception - unable to locate the control
+
+//No :4 -----Invalid Selector exception -- The value of locator is in wrong format
+
+//No:5  ------Element Click intersepted exception---
+//No :6 ------Null pointer Exeception --whenever the object is not initialized,that time we aill get this.
+// 7----------MethodMatcher exception ----If the method is not matching we will get this kind of exception
